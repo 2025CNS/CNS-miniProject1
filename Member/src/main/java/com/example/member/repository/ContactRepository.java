730d95230b3f8ead954459a7a5d6f9860848bbe1
itzjb <146229370+itzjb@users.noreply.github.com>
2025-08-11 21:41:31 +0900

    set

--- Member/src/main/java/com/example/member/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.member.repository;
-
-
-import com.example.member.entity.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
-
